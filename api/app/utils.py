da2d0d8b7353f2315b309dd3888120866e99329e
Vadym Karachenko <v1dr00x@gmail.com>
2021-06-02 17:31:23 +0300

    Remove debug comment

--- api/app/utils.py
+++ api/app/utils.py
@@ -22,19 +22,12 @@ async def preflight_setup():
     db.raw_engine = client[settings.DATABASE_NAME]
     db.engine = engine
     db.database = engine.database
+    
     try:
         await create_default_access_levels()
     except AccessLevelExists:
         logger.info('Default access levels exist, continuing...')
-    '''
-    await create_admin_user(
-        email="testadmin1@example.com",
-        first_name="Super",
-        last_name="Admin",
-        middle_name="User 1",
-        password="123321",
-    )
-    '''
+    
     logger.info("Initial setup completed successfully!")
 
 
